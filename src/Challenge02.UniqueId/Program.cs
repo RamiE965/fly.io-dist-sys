cadbd8666ddedd48945bc4c8e6c57a4214fa9794
Rami Elsayed <ramielsayed@Ramis-MacBook-Air.local>
2025-11-12 17:10:29 -0600

    Finish Challenge 2

---
+++ src/Challenge02.UniqueId/Program.cs
@@ -0,0 +1,21 @@
+﻿using Maelstrom.Core;
+
+var node = new Node();
+int counter = 0;
+
+node.Handle("generate", async (Message msg) =>
+{
+    int id = Interlocked.Increment(ref counter); // atomic increment
+    
+    string unqiueId = $"{node.NodeId}-{id}";
+    
+    var body = new Dictionary<string, object>
+    {
+        { "type", "generate_ok" },
+        { "id", unqiueId }
+    };
+    
+    await node.Reply(msg, body);
+});
+
+await node.Run();
